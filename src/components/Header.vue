737e85045e28ac05ec062ad35587ecbb5d71fa96
Adino Chang <adinochang@users.noreply.github.com>
2025-06-15 13:31:58 +0800

    Add logout functionality

---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { useUserStore } from '../stores/user'
+import { useRouter } from 'vue-router'
+
+import robotImage from '../assets/robot.png'
+
+// user store
+const userStore = useUserStore();
+
+// logout function
+const logoutUser = () => {
+    userStore.logout()
+    router.push('/')
+}
+
+
+// perform page navigation
+const router = useRouter();
+
+</script>
+
+<template>
+    <div class="py-4 px-6 bg-gray-800 shadown-md flex justify-between items-center">
+        <img
+            :src="robotImage"
+            alt="Chat AI"
+            class="w-8 h-8"
+        />
+        <h1 class="text-2xl font-semibild-mb-4 text-center">
+            Welcome to Chat AI
+        </h1>
+        <button
+            class="text-gray-400 hover:text-white"
+            @click="logoutUser"
+        >
+            Logout
+        </button>
+    </div>
+</template>
--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -1,5 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Header from '../components/Header.vue'
+</script>
 
 <template>
-    <h1>Chat Page</h1>
+    <div class="flex flex-col h-screen bg-gray-900 text-white">
+        <Header />
+    </div>
+    <h1 class="text-lg font-semibold">Chat Page</h1>
 </template>
