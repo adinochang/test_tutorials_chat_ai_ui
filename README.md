94c48675a7da0b20b0283c98a3a0b1cd7aa448b3
Adino Chang <adinochang@users.noreply.github.com>
2025-06-15 10:43:58 +0800

    Init repo

---
+++ README.md
@@ -0,0 +1,8 @@
+# Notes
+
+
+
+
+# Video
+
+https://youtu.be/VR3p7almo_c?feature=shared
---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <h1>My App</h1>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
